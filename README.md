cb538175a5a8a738361771613bad3613674e557d
Phyllis Douglas <goofanader@gmail.com>
2015-02-06 13:50:11 -0800

    Loading in Classes
    I'm going to use middleclass and middleclass-commons by kikito. I won't
    include it in my github stuff because you can just get it off of
    github...

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # lovely-things
 Programming projects in LOVE 2D.
 
+Please note that many of these projects require kikito's wonderful "middleclass" and "middleclass-commons" lua scripts.
+
 ## dining-philosophers
 Demonstration of the Dining Philosophers problem, but with some nice pixel visuals. Initially, there will be 5 philosophers and 5 forks. I hope to make it so that a variable number of philosophers and forks will be possible.
--- dining-philosophers/main.lua
+++ dining-philosophers/main.lua
@@ -1,8 +1,27 @@
+require 'philosopher'
+
+maxFrameRate = 12 --12 frames per second
+totalFrames = 0
+wantsPrint = false
+
+NUM_PHILOSOPHERS = 5
+NUM_FORKS = NUM_PHILOSOPHERS
+
 function love.load()
 end
 
 function love.draw()
+   if wantsPrint then
+      -- print the status of the threads
+   end
 end
 
 function love.update(dt)
+   totalFrames = totalFrames + dt
+   
+   if totalFrames >= maxFrameRate then
+      totalFrames = 0
+      
+      -- update the graphics, update philosopher decisions
+   end
 end
